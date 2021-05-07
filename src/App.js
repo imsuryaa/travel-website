4e2ecbc39b979cc961727abd3b8a4bcacad82bcf
suryatejaj97 <suryatejaj97@gmail.com>
2021-05-07 18:14:26 +0530

    added with navbar and hero section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home'
 function App() {
   return (
     <div className="App">
-      <h1>Code clean up</h1>      
+    <Router>
+    <Navbar />  
+      <Switch>
+        <Route path='/' exact component={Home}/>
+      </Switch>     
+      </Router>
     </div>
   );
 }
